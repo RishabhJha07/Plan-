9ca96251748b143d970376cff4e1a489b47a173d
RishabhJha07 <rishabhjha5017@gmail.com>
2025-10-01 09:09:42 +0530

    fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,7 +11,7 @@ function Card({id,image,info,price,name,removeTour}){
 
     return(
         <div className="Card">
-            <img src={image} className="image"></img>
+            <img src={image} className="image" alt=""></img>
             
             <div className="tour-info">
                 <div className="tour-details">
